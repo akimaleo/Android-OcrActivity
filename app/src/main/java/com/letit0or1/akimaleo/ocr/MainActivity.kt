e4fd34ab6142103a14903dda54e9066debbd7efe
Yaroslav <akimaleo@ya.ru>
2017-11-25 17:32:34 +0200

    New design was implemented

---
+++ app/src/main/java/com/letit0or1/akimaleo/ocr/MainActivity.kt
@@ -0,0 +1,111 @@
+package com.letit0or1.akimaleo.ocr
+
+import android.app.Activity
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.provider.MediaStore
+import android.support.v7.app.AppCompatActivity
+import android.text.method.KeyListener
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import com.theartofdev.edmodo.cropper.CropImage
+import com.theartofdev.edmodo.cropper.CropImageView
+import it.sephiroth.android.library.tooltip.Tooltip
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.IOException
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var keyListener: KeyListener
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        take_photo.setOnClickListener {
+            CropImage.activity()
+                    .setGuidelines(CropImageView.Guidelines.ON)
+                    .start(this@MainActivity)
+        }
+
+        keyListener = data.keyListener
+        setEditable(false)
+        set_editable.setOnClickListener {
+            changeEditable()
+        }
+
+        copy.setOnClickListener {
+            val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clip = ClipData.newPlainText("ocr", data.text)
+            clipboard.primaryClip = clip
+            Toast.makeText(this@MainActivity, "Copied to clipboard", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun hideSoftwareKeyboard() {
+        val view = this.currentFocus
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
+        imm?.hideSoftInputFromWindow(view?.windowToken, 0)
+    }
+
+    private fun changeEditable() {
+        setEditable(check.visibility == View.INVISIBLE)
+    }
+
+    private fun setEditable(setEditable: Boolean) {
+        if (setEditable) {
+            check.visibility = View.VISIBLE
+            data.keyListener = keyListener
+        } else {
+            check.visibility = View.INVISIBLE
+            data.keyListener = null
+            hideSoftwareKeyboard()
+        }
+    }
+
+    private fun showCopyToolTip() {
+        Tooltip.make(this,
+                Tooltip.Builder(100)
+                        .anchor(copy, Tooltip.Gravity.TOP)
+                        .closePolicy(Tooltip.ClosePolicy()
+                                .insidePolicy(true, false)
+                                .outsidePolicy(true, false), 2000)
+                        .activateDelay(800)
+                        .showDelay(300)
+                        .text("Copy text")
+                        .maxWidth(500)
+                        .withArrow(true)
+                        .withOverlay(true)
+                        .floatingAnimation(Tooltip.AnimationBuilder.DEFAULT)
+                        .build()
+        ).show()
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, dataIntent: Intent) {
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            val result = CropImage.getActivityResult(dataIntent)
+            if (resultCode == Activity.RESULT_OK) {
+                val resultUri = result.uri
+                var bitmap: Bitmap? = null
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, resultUri)
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+
+                val q = com.letit0or1.akimaleo.opticalcharacterrecognizer.OpticalCharacterRecognizer.extractText(bitmap, this)
+                data.setText(q)
+                showCopyToolTip()
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                val error = result.error
+            }
+        }
+    }
+}
--- app/src/main/java/com/letit0or1/akimaleo/ocr/OpticalCharacterRecognizer.java
+++ app/src/main/java/com/letit0or1/akimaleo/ocr/OpticalCharacterRecognizer.java
@@ -8,63 +8,59 @@ import android.util.Log;
 import android.util.SparseArray;
 import android.widget.Toast;
 
-import com.google.android.gms.vision.Frame;
-import com.google.android.gms.vision.text.TextBlock;
-import com.google.android.gms.vision.text.TextRecognizer;
-
 /**
  * Created by akimaleo on 11.01.17.
  */
 
-public class OpticalCharacterRecognizer {
-
-    public static String LOG_TAG = "OCR";
-//    public static String DATA_PATH = "src/main/assets/tessdata";
-
-    public static String extractText(Bitmap imageBitmap, Context context) {
-        String extractedText = "";
-        // imageBitmap is the Bitmap image you're trying to process for text
-        if (imageBitmap != null) {
-
-            TextRecognizer textRecognizer = new TextRecognizer.Builder(context).build();
-
-            if (!textRecognizer.isOperational()) {
-                // Note: The first time that an app using a Vision API is installed on a
-                // device, GMS will download a native libraries to the device in order to do detection.
-                // Usually this completes before the app is run for the first time.  But if that
-                // download has not yet completed, then the above call will not detect any text,
-                // barcodes, or faces.
-                // isOperational() can be used to check if the required native libraries are currently
-                // available.  The detectors will automatically become operational once the library
-                // downloads complete on device.
-                Log.w(LOG_TAG, "Detector dependencies are not yet available.");
-
-                // Check for low storage.  If there is low storage, the native library will not be
-                // downloaded, so detection will not become operational.
-                IntentFilter lowstorageFilter = new IntentFilter(Intent.ACTION_DEVICE_STORAGE_LOW);
-                boolean hasLowStorage = context.registerReceiver(null, lowstorageFilter) != null;
-
-                if (hasLowStorage) {
-                    Toast.makeText(context, "Low Storage", Toast.LENGTH_LONG).show();
-                    Log.w(LOG_TAG, "Low Storage");
-                }
-            }
-
-
-            Frame imageFrame = new Frame.Builder()
-                    .setBitmap(imageBitmap)
-                    .build();
-
-            SparseArray<TextBlock> textBlocks = textRecognizer.detect(imageFrame);
-
-            for (int i = 0; i < textBlocks.size(); i++) {
-                TextBlock textBlock = textBlocks.get(textBlocks.keyAt(i));
-                extractedText += textBlock.getValue();
-                Log.e(LOG_TAG, textBlock.getValue());
-                // Do something with value
-            }
-        }
-        return extractedText;
-    }
-
-}
+//public class OpticalCharacterRecognizer {
+//
+//    public static String LOG_TAG = "OCR";
+////    public static String DATA_PATH = "src/main/assets/tessdata";
+//
+//    public static String extractText(Bitmap imageBitmap, Context context) {
+//        String extractedText = "";
+//        // imageBitmap is the Bitmap image you're trying to process for text
+//        if (imageBitmap != null) {
+//
+//            TextRecognizer textRecognizer = new TextRecognizer.Builder(context).build();
+//
+//            if (!textRecognizer.isOperational()) {
+//                // Note: The first time that an app using a Vision API is installed on a
+//                // device, GMS will download a native libraries to the device in order to do detection.
+//                // Usually this completes before the app is run for the first time.  But if that
+//                // download has not yet completed, then the above call will not detect any text,
+//                // barcodes, or faces.
+//                // isOperational() can be used to check if the required native libraries are currently
+//                // available.  The detectors will automatically become operational once the library
+//                // downloads complete on device.
+//                Log.w(LOG_TAG, "Detector dependencies are not yet available.");
+//
+//                // Check for low storage.  If there is low storage, the native library will not be
+//                // downloaded, so detection will not become operational.
+//                IntentFilter lowstorageFilter = new IntentFilter(Intent.ACTION_DEVICE_STORAGE_LOW);
+//                boolean hasLowStorage = context.registerReceiver(null, lowstorageFilter) != null;
+//
+//                if (hasLowStorage) {
+//                    Toast.makeText(context, "Low Storage", Toast.LENGTH_LONG).show();
+//                    Log.w(LOG_TAG, "Low Storage");
+//                }
+//            }
+//
+//
+//            Frame imageFrame = new Frame.Builder()
+//                    .setBitmap(imageBitmap)
+//                    .build();
+//
+//            SparseArray<TextBlock> textBlocks = textRecognizer.detect(imageFrame);
+//
+//            for (int i = 0; i < textBlocks.size(); i++) {
+//                TextBlock textBlock = textBlocks.get(textBlocks.keyAt(i));
+//                extractedText += textBlock.getValue();
+//                Log.e(LOG_TAG, textBlock.getValue());
+//                // Do something with value
+//            }
+//        }
+//        return extractedText;
+//    }
+//
+//}
